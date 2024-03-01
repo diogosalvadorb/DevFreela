e4393d997648c456f2074d0a769fd1490d4edb9e
diogosalvadorb <contact.diogosalvador@gmail.com>
2024-02-29 21:37:02 -0300

    validações ponto de acessos api

--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -1,7 +1,10 @@
+using DevFreela.API.Filters;
 using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Application.Validators;
 using DevFreela.Core.Repositories;
 using DevFreela.Infrastructure.Persistence;
 using DevFreela.Infrastructure.Persistence.Repositories;
+using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -35,7 +38,9 @@ namespace DevFreela.API
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ISkillRepository, SkillRepository>();
 
-            services.AddControllers();
+            services.AddControllers(options => options.Filters.Add(typeof(ValidationFilter)))
+               .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());
+
 
             services.AddMediatR(typeof(CreateProjectCommand));
 
